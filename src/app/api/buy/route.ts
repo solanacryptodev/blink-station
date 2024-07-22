8ec38ab3cc3ee94b96dc1f15a39c04c3112e9ef5
solanacryptodev <eoscrypto87@protonmail.com>
2024-07-22 14:11:02 -0400

    Able to trigger order list via blink button click

--- src/app/api/buy/route.ts
+++ src/app/api/buy/route.ts
@@ -1,7 +1,14 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { PublicKey } from '@solana/web3.js';
-import { ActionGetResponse, ActionPostResponse, ACTIONS_CORS_HEADERS } from "@solana/actions";
-import { PROGRAM_ID, CONNECTION, gmClientService, isValidNftName, getNftMint } from '@/lib/blinks/actions';
+import { ActionGetResponse, ActionPostRequest, ActionPostResponse, ACTIONS_CORS_HEADERS } from "@solana/actions";
+import {
+    PROGRAM_ID,
+    CONNECTION,
+    gmClientService,
+    isValidNftName,
+    getNftMint,
+    validatedQueryParams
+} from '@/lib/blinks/actions';
 import { BN } from "@coral-xyz/anchor";
 
 export const dynamic = 'force-dynamic'
@@ -9,28 +16,26 @@ export const dynamic = 'force-dynamic'
 export const GET = async (req: Request) => {
     try {
         const requestURL = new URL(req.url);
-        console.log('searchParams: ', requestURL.origin);
-        // console.log('nft name: ', searchParams.get('nftName'));
-        // const nftName = searchParams.get('nftName')?.toLowerCase() as string;
-        const nftName = 'pearce_x4';
+        console.log('requestURL: ', requestURL.origin);
+        const { playerPubKey } = validatedQueryParams(requestURL);
 
         // if (!nftName || !isValidNftName(nftName)) {
         //     return Response.json({ error: 'Invalid or missing NFT name' },
         //     { headers: ACTIONS_CORS_HEADERS, status: 400 });
         // }
 
-        const baseHref = new URL(`/api/buy?nftName=${nftName}`, requestURL.origin).toString();
+        const baseHref = new URL(`/api/buy?player=${playerPubKey}`, requestURL.origin).toString();
 
         const payload: ActionGetResponse = {
             title: "Star Atlas NFT Purchase",
             icon: "https://staratlas.com/favicon.ico",
             description: `Purchase an NFT from the Star Atlas marketplace`,
-            label: "Select NFT Order",
+            label: "FindOrdersForAsset",
             links: {
                 actions: [
                     {
                         label: "Find Orders",
-                        href: `${baseHref}&action=findOrders`,
+                        href: `${baseHref}&nftName={nftName}`,
                         parameters: [
                             {
                                 name: "nftName",
@@ -43,76 +48,77 @@ export const GET = async (req: Request) => {
             }
         };
 
-        console.log('payload: ', payload.links?.actions[0]);
-        return Response.json(payload, { headers: ACTIONS_CORS_HEADERS });
+        return NextResponse.json(payload, {
+            headers: ACTIONS_CORS_HEADERS
+        });
     } catch ( error ) {
         console.log('error: ', error);
+        return new NextResponse(
+            'Invalid request',
+            { headers: ACTIONS_CORS_HEADERS, status: 400 }
+        );
     }
-
-    return NextResponse.json({ error: 'Invalid or missing NFT name' }, { headers: ACTIONS_CORS_HEADERS, status: 400 });
 }
 
+export const OPTIONS = GET;
+
 export const POST = async (req: Request) => {
-    const requestURL = new URL(req.url);
-    const nftName = 'pearce_x4';
-    const action = requestURL.searchParams.get('action');
-    const orderId = requestURL.searchParams.get('orderId');
-    const orderIdKey = new PublicKey(orderId as string);
+    try {
+        console.log('connection: ', CONNECTION);
+        console.log('programID: ', PROGRAM_ID.toString());
+        const requestURL = new URL(req.url);
+        const { nftName, label } = validatedQueryParams(requestURL);
+        console.log('nft name: ', nftName);
+        console.log('performing action... ', requestURL);
+        // const orderId = requestURL.searchParams.get('orderId');
+        // const orderIdKey = new PublicKey(orderId as string);
 
-    if (!nftName || !isValidNftName(nftName)) {
-        return Response.json({ error: 'Invalid or missing NFT name' }, { headers: ACTIONS_CORS_HEADERS, status: 400 });
-    }
+        const body: ActionPostRequest = await req.json();
+        console.log('body: ', body);
+        const buyerPubkey = new PublicKey(body.account);
 
-    const body = await req.json();
-    console.log('body: ', body);
-    const buyerPubkey = new PublicKey(body.account);
+        const nftMint = getNftMint(nftName) as PublicKey;
+        console.log('nftMint: ', nftMint.toString());
 
-    const nftMint = getNftMint(nftName);
-    if (!nftMint) {
-        return Response.json({ error: 'Invalid NFT name' }, { headers: ACTIONS_CORS_HEADERS, status: 400 });
-    }
+        const orders = await gmClientService.getOpenOrdersForAsset(CONNECTION, nftMint, PROGRAM_ID);
+        const buyOrders = orders.filter(order => order.orderType === 'buy');
+        console.log('buy orders: ', buyOrders);
 
-    if (action === 'findOrders') {
-        const orders = await gmClientService.getOpenOrdersForAsset(CONNECTION, PROGRAM_ID, nftMint);
-        console.log('orders: ', orders);
-        const topOrders = orders.slice(0, 6).map(order => ({
-            label: `Buy for ${order.uiPrice} ATLAS`,
-            href: `/api/buy?nftName=${nftName}&action=buy&orderId=${order.id}`
+        const topOrders = buyOrders.slice(0, 6).map(order => ({
+            label: `Buy for ${order.uiPrice} ${order.currencyMint === 'ATLASXmbPQxBUYbxPsV97usA3fPQYEqzQBUHgiFCUsXx' ? 'ATLAS' : 'USDC'}`,
+            href: `/api/buy?nftName=${nftName}&action=buy`
         }));
 
+        console.log('topOrders: ', topOrders);
+
         const payload: ActionGetResponse = {
-            title: "Star Atlas NFT Purchase",
-            icon: "https://staratlas.com/favicon.ico",
-            description: `Select an order to purchase ${nftName.toUpperCase()} NFT`,
-            label: "Select Order",
-            links: { actions: topOrders }
-        };
+                title: "Star Atlas NFT Purchase",
+                icon: "https://staratlas.com/favicon.ico",
+                description: `Select an order to purchase ${nftName.toUpperCase()} NFT`,
+                label: "Select Order",
+                links: { actions: topOrders }
+            };
 
-        return Response.json(payload, { headers: ACTIONS_CORS_HEADERS });
-    } else if (action === 'buy' && orderId) {
-        const order = await gmClientService.getOpenOrder(CONNECTION, PROGRAM_ID, orderIdKey);
-        if (!order) {
-            return Response.json({ error: 'Order not found' }, { headers: ACTIONS_CORS_HEADERS, status: 400 });
-        }
+            // const purchaseQty = 1;
+            // const exchangeTx = await gmClientService.getCreateExchangeTransaction(
+            //     CONNECTION,
+            //     orders[0],
+            //     buyerPubkey,
+            //     purchaseQty,
+            //     PROGRAM_ID,
+            // );
+            //
+            // const serializedTransaction = exchangeTx.transaction.serialize({ requireAllSignatures: false }).toString('base64');
+            //
+            // const second_payload: ActionPostResponse = {
+            //     transaction: serializedTransaction,
+            //     message: `Purchase ${nftName.toUpperCase()} NFT for ${orders[0].uiPrice} ATLAS`
+            // };
 
-        const purchaseQty = 1;
-        const exchangeTx = await gmClientService.getCreateExchangeTransaction(
-            CONNECTION,
-            order,
-            buyerPubkey,
-            purchaseQty,
-            PROGRAM_ID,
+        return NextResponse.json(payload, { headers: ACTIONS_CORS_HEADERS }
         );
 
-        const serializedTransaction = exchangeTx.transaction.serialize({ requireAllSignatures: false }).toString('base64');
-
-        const payload: ActionPostResponse = {
-            transaction: serializedTransaction,
-            message: `Purchase ${nftName.toUpperCase()} NFT for ${order.uiPrice} ATLAS`
-        };
-
-        return Response.json(new BN(payload), { headers: ACTIONS_CORS_HEADERS });
-    } else {
-        return Response.json({ error: 'Invalid action' }, { headers: ACTIONS_CORS_HEADERS, status: 400 });
+    } catch ( error ) {
+        console.log('error: ', error);
     }
 }
--- src/lib/blinks/actions.ts
+++ src/lib/blinks/actions.ts
@@ -7,8 +7,8 @@ export const PROGRAM_ID = new PublicKey('traderDnaR5w6Tcoi3NFm53i48FTDNbGjBSZwWX
 export const CONNECTION = new Connection(process.env.HELIUS_RPC!);
 
 export const NFT_NAME_TO_MINT: { [key: string]: string } = {
-    'pearce_x4': '2iMhgB4pbdKvwJHVyitpvX5z1NBNypFonUgaSAt9dtDt',
-    // Add more NFTs here
+    'pearce x4': '2iMhgB4pbdKvwJHVyitpvX5z1NBNypFonUgaSAt9dtDt',
+    'calico maxhog': 'GxpbUDxYYvxiUejHcAMzeV2rzdHf6KZZvT86ACrpFgXa',
 };
 
 export const gmClientService = new GmClientService();
@@ -20,4 +20,37 @@ export function isValidNftName(nftName: string): boolean {
 export function getNftMint(nftName: string): PublicKey | null {
     const mint = NFT_NAME_TO_MINT[nftName.toLowerCase()];
     return mint ? new PublicKey(mint) : null;
+}
+
+export function validatedQueryParams(requestUrl: URL) {
+    let playerPubKey = new PublicKey('5YMeDBj2C41Fr6paRWUdKc7dXua1DTbhhQwkaKuDdzty');
+    let nftName: string = '';
+    let label: string = '';
+
+    try {
+        if (requestUrl.searchParams.get("player")) {
+            playerPubKey = new PublicKey(requestUrl.searchParams.get("player")!);
+        }
+    } catch (err) {
+        new Error('Invalid player');
+    }
+
+    try {
+        if (requestUrl.searchParams.get("nftName")) {
+            nftName = requestUrl.searchParams.get("nftName") as string;
+            console.log('requestURL: ', requestUrl.toJSON());
+        }
+
+        if (!isValidNftName(nftName)) {
+            new Error('Invalid NFT name');
+        }
+    } catch (err) {
+        throw "Invalid input query parameter: amount";
+    }
+
+    return {
+        nftName,
+        playerPubKey,
+        label
+    };
 }
